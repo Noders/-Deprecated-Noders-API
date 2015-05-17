3ae2d0dbe4eed360e48053df358f6348d40f1181
fforres <felipe.torressepulveda@gmail.com>
2015-05-16 21:54:02 -0400

    adding users, models: evento, host, noder, partner,followinghost, followingnoder, acls creation, etc

---
+++ server/boot/user.js
@@ -0,0 +1,148 @@
+module.exports = function(app) {
+    var _ = require('lodash');
+    /*
+     * The `app` object provides access to a variety of LoopBack resources such as
+     * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+     * `app.datasources.YourDataSource`). See
+     * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+     * for more info.
+     */
+    var Noder = app.models.Noder;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    function createUsers() {
+        //SLxbsTbCtBUMmeDSfXUda0XMl5UMqAJk40s5H2KVeAjuXBlFCOcZbDL1QNV8jf39
+        Noder.upsert([{
+            "Nombre": "Felipe",
+            "Apellido": "Torres",
+            "Pais": "Chile",
+            "img": "fforres.jpg",
+            "url": "http://www.twitter.com/fforres",
+            "username_twitter": "fforres",
+            "username_github": "fforres",
+            "username": "fforres",
+            "email": "felipe.torressepulveda@gmail.com",
+            "password": "founder",
+            "hostId": "5557bec9fa0f567413db2dfc"
+        }, {
+            "pais": "Chile",
+            "img": "josevildosola.jpg",
+            "username": "josevildosola55",
+            "hostId": "5557bec9fa0f567413db2dfc",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/josevildosola55"
+        }, {
+            "pais": "Chile",
+            "img": "maetschl.jpeg",
+            "username": "maetschl",
+            "hostId": "5557bec9fa0f567413db2dfc",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/maetschl"
+        }, {
+            "pais": "Ecuador",
+            "img": "rchancay.jpeg",
+            "username": "r_chancay",
+            "hostId": "5557bedafa0f567413db2dfd",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/r_chancay"
+        }, {
+            "pais": "El Salvador",
+            "img": "AdrianoChiliseo.jpeg",
+            "username": "AdrianoChiliseo",
+            "hostId": "5557bedffa0f567413db2dfe",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/AdrianoChiliseo"
+        }, {
+            "pais": "El Salvador",
+            "img": "WillBonilla11.jpg",
+            "username": "WillBonilla11",
+            "hostId": "5557bedffa0f567413db2dfe",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/WillBonilla11"
+        }, {
+            "pais": "El Salvador",
+            "img": "enriquegraficos.png",
+            "username": "enriquegraficos",
+            "hostId": "5557bedffa0f567413db2dfe",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/enriquegraficos"
+        }, {
+            "pais": "El Salvador",
+            "img": "melvingilbertos.jpg",
+            "username": "melvingilbertos",
+            "hostId": "5557bedffa0f567413db2dfe",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/melvingilbertos"
+        }, {
+            "pais": "El Salvador",
+            "img": "norr1994.jpg",
+            "username": "norr1994",
+            "hostId": "5557bedffa0f567413db2dfe",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/norr1994"
+        }, {
+            "pais": "Argentina",
+            "img": "beacon_tech.png",
+            "username": "beacon_tech",
+            "hostId": "5557bee3fa0f567413db2dff",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/beacon_tech"
+        }, {
+            "pais": "Colombia",
+            "img": "icristiam.jpeg",
+            "username": "icristiam",
+            "hostId": "5557bee7fa0f567413db2e00",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/icristiam"
+        }, {
+            "pais": "Colombia",
+            "img": "Juanqtx.jpg",
+            "username": "Juanqtx",
+            "hostId": "5557bee7fa0f567413db2e00",
+            "email": "test@test.com",
+            "password": "test",
+            "url": "http://www.twitter.com/Juanqtx"
+        }], function(err, users) {
+            if (err) return console.log(err);
+            Role.create({
+                name: 'ADMIN'
+            }, function(err, role) {
+                _.each(users, function(user, i) {
+                    role.principals.create({
+                        principalType: RoleMapping.USER,
+                        principalId: user.id
+                    }, function(err, principal) {
+                        if (err) return console.log(err);
+                        console.log(principal);
+                    });
+                })
+            });
+
+            Role.create({
+                name: 'FOUNDER'
+            }, function(err, role) {
+                role.principals.create({
+                    principalType: RoleMapping.USER,
+                    principalId: users[0].id
+                }, function(err, principal) {
+                    if (err) return console.log(err);
+                    console.log(principal);
+                });
+
+            });
+
+        });
+    }
+    createUsers();
+};
